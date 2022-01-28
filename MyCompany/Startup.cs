a6d96d69dbc8ee913bd42ce7a9eeb8774aa3ac98
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2022-01-28 16:05:05 +0300

    Change startup

--- MyCompany/Startup.cs
+++ MyCompany/Startup.cs
@@ -12,13 +12,14 @@ namespace MyCompany
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            //Добавляем поддержку контроллеров и представлений (MVC)
+            services.AddControllersWithViews()
+                //выстовляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -28,12 +29,11 @@ namespace MyCompany
 
             app.UseRouting();
 
+            app.UseStaticFiles();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
