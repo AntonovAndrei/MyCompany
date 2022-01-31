86b933c2b19ede29dbfa62066bac952339de1812
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2022-01-31 17:10:49 +0300

    Add repository

--- MyCompany/Startup.cs
+++ MyCompany/Startup.cs
@@ -1,9 +1,15 @@
-using Microsoft.AspNetCore.Builder;
+ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MyCompany.Domain;
+using MyCompany.Domain.Repositories.Abstract;
+using MyCompany.Domain.Repositories.EntityFramework;
 using MyCompany.Service;
 using System;
 using System.Collections.Generic;
@@ -22,10 +28,45 @@ namespace MyCompany
             //подключаем конфиг из appsetting.json
             Configuration.Bind("Project", new Config());
 
-            //Добавляем поддержку контроллеров и представлений (MVC)
+            //подключаем нужный функционал приложения в качестве сервисов
+            services.AddTransient<ITextFieldsRepository, EFTextFieldsRepository>();
+            services.AddTransient<IServiceItemsRepository, EFServiceItemsRepository>();
+            services.AddTransient<DataManager>();
+
+            //подключаем контекст
+            services.AddDbContext<AppDbContext>(x => x.UseSqlServer(Config.ConnectionString));
+
+            //настраиваем identity
+            services.AddIdentity<IdentityUser, IdentityRole>(opts =>
+            {
+                opts.User.RequireUniqueEmail = true;
+                opts.Password.RequiredLength = 6;
+                opts.Password.RequireNonAlphanumeric = false;
+                opts.Password.RequireLowercase = false;
+                opts.Password.RequireUppercase = false;
+                opts.Password.RequireDigit = false;
+            }).AddEntityFrameworkStores<AppDbContext>().AddDefaultTokenProviders();
+
+            //настраиваем authentication cookie
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.Cookie.Name = "myCompanyAuth";
+                options.Cookie.HttpOnly = true;
+                options.LoginPath = "/account/login";
+                options.AccessDeniedPath = "/account/accessdenied";
+                options.SlidingExpiration = true;
+            });
+
+            //настраиваем политику авторизации для Admin area
+            services.AddAuthorization(x =>
+            {
+                x.AddPolicy("AdminArea", policy => { policy.RequireRole("admin"); });
+            });
+
+            //добавляем сервисы для контроллеров и представлений (MVC)
             services.AddControllersWithViews()
-                //выстовляем совместимость с asp.net core 3.0
-                .SetCompatibilityVersion(Microsoft.AspNetCore.Mvc.CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
+                //выставляем совместимость с asp.net core 3.0
+                .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
         
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -38,6 +79,10 @@ namespace MyCompany
             app.UseRouting();
 
             app.UseStaticFiles();
+
+            app.UseCookiePolicy();
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
